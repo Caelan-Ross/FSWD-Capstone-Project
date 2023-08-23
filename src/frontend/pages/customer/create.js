4b226f04032146c92bbda9c1931d2bb9570cfac0
Larnerk <97647756+Larnerk@users.noreply.github.com>
2023-08-23 09:58:28 -0600

    Remove extraneous import call on navbar component. Fix error for variable name in Customer Create.

--- src/frontend/pages/customer/create.js
+++ src/frontend/pages/customer/create.js
@@ -77,7 +77,7 @@ export default function Home() {
 					width: '100%',
 				}}
 			>
-				{error && <Alert severity='error'>{error}</Alert>}
+				{isError && <Alert severity='error'>{isError}</Alert>}
 				<Typography variant='h3' align='center' component='h2'>
 					Create Customer
 				</Typography>
